1928f5fbf16129f6f4a48dfc20ba34e6fdc489b7
SperanskyArtyom <awesomepinkieguy@gmail.com>
2026-02-20 13:55:26 +0700

    add UserFilter.and, UserFilter.or default methods in UserFilter functional interface

--- src/main/types/interfaces/functional/UserFilter.java
+++ src/main/types/interfaces/functional/UserFilter.java
@@ -5,4 +5,20 @@ import types.records.User;
 @FunctionalInterface
 public interface UserFilter {
     boolean test(User user);
+
+    default UserFilter and(UserFilter other) {
+        if (other == null) {
+            throw new IllegalArgumentException("Other filter must not be null");
+        }
+
+        return user -> test(user) && other.test(user);
+    }
+
+    default UserFilter or(UserFilter other) {
+        if (other == null) {
+            throw new IllegalArgumentException("Other filter must not be null");
+        }
+
+        return user -> test(user) || other.test(user);
+    }
 }
